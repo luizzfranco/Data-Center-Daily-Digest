fc1a6969f90468b17db4386a6b623cd440050180
Luiz Franco <65632456+luizzfranco@users.noreply.github.com>
2026-03-05 21:02:23 -0300

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -33,7 +33,7 @@ def save_to_sheets(articles_br, articles_global, digest):
         tags = ", ".join(article.get("tags", []))
         resumo = br_summaries.get(article["url"], "")
         rows.append([
-            "/".join(reversed(article["date"].split("-"))),
+            article["date"],
             "BR",
             article["title"],
             article["url"],
@@ -51,7 +51,7 @@ def save_to_sheets(articles_br, articles_global, digest):
         tags = ", ".join(article.get("tags", []))
         resumo = global_summaries.get(article["url"], "")
         rows.append([
-            "/".join(reversed(article["date"].split("-"))),
+            article["date"],
             "Global",
             article["title"],
             article["url"],
